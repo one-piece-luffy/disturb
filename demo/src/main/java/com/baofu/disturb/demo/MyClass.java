d9f6b3147fcd1706f28368f5bd6059a00b5df343
one piece <646796992@qq.com>
2025-03-20 17:09:00 +0800

    1

--- demo/src/main/java/com/baofu/disturb/demo/MyClass.java
+++ demo/src/main/java/com/baofu/disturb/demo/MyClass.java
@@ -1,8 +1,20 @@
 package com.baofu.disturb.demo;
 
+import com.baofu.disturb.Guard;
+
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Guard guard=new Guard("D:\\project\\git\\disturb\\app");
+//        //修改包名
+        guard.changePackageName("com.example.myapplication","c.e.m");
+////        //修改类名
+        guard.changeClassName();
+////        //重命名布局文件
+        guard.changeLayoutName();
+////        //重命名图片
+        guard.changeDrawableName();
+        //修改colors.xml的name
+        guard.changeColorAttrName();
 
     }
 }
