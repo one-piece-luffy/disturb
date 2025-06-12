f7126927e01d304a9682366bd8f988cda2c312fd
one piece <646796992@qq.com>
2025-06-12 10:19:15 +0800

    1

--- lib/src/main/java/com/baofu/disturb/MainClass.java
+++ lib/src/main/java/com/baofu/disturb/MainClass.java
@@ -4,8 +4,11 @@ public class MainClass {
     public static void main(String asdf[]) {
 
         GuardHelper guard=new GuardHelper("C:\\Users\\Administrator\\Documents\\GitHub\\SaveTiktok\\app");
+        //皮肤资源res名称
         guard.skinResPath="res-dark";
+        //皮肤资源前缀
         guard.begin="skin_";
+        //皮肤资源后缀
         guard.end="_dark";
 //        //修改包名
         guard.changePackageName("com.instagram.ig","t.t.g");
